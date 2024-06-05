0b80925610cfb67e94ad59f93b80304290632a4d
Yogesh Nepal <yogesh.nepal@nepalpayment.com>
2024-06-05 16:27:17 +0545

    Added rate limiting using AspNetCorerateLimit library

--- rate.limit.middleware.example/Program.cs
+++ rate.limit.middleware.example/Program.cs
@@ -1,5 +1,7 @@
 
+using AspNetCoreRateLimit;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using rate.limit.middleware.example.DatabaseContext;
 using rate.limit.middleware.example.RateLimitingMiddleware;
 
@@ -26,6 +28,24 @@ namespace rate.limit.middleware.example
             {
                 options.UseSqlServer(connectionString); // Replace with your database provider (e.g., UseMySQL, UseNpgsql, etc.)
             });
+
+            #region for ratelimit using aspnetcoreratelimit
+            // Load configuration from appsettings.json
+            builder.Services.AddOptions();
+            builder.Services.AddMemoryCache();
+            // Load general configuration
+            builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+
+            // Load IP rules from appsettings.json
+            builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+
+            builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+            // Add rate limiting
+            builder.Services.AddInMemoryRateLimiting();
+            #endregion
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -42,6 +62,9 @@ namespace rate.limit.middleware.example
             // Use the rate limit middleware
             app.UseRateLimitMiddleware();
             app.UseRateLimitMiddlewareUsingDatabase();
+
+            // Enable IP rate limiting middleware for ratelimit using aspnetcoreratelimit
+            app.UseIpRateLimiting();
 
 
             app.MapControllers();
